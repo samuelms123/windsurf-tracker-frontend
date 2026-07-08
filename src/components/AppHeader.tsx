9731fe7227fb3181712517f6a5b0c16cda667115
Samuel Sarimo <samuel.sarimo@gmail.com>
2026-07-08 15:24:30 +0300

    Correct pwa bg color

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -7,7 +7,7 @@ type HeaderProps = {
 
 export default function AppHeader({ title, action }: HeaderProps) {
   return (
-    <header className="h-20 w-full shrink-0 bg-slate-950 px-6">
+    <header className="pt-[env(safe-area-inset-top)] w-full shrink-0 bg-slate-950 px-6">
       <div className="flex h-full w-full items-center justify-between border-b border-slate-800/40">
         <h1 className="text-3xl font-bold tracking-tight text-white select-none">
           {title}
